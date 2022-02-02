61a7e73ae7f6f64438f8e920b6ba7713fef33536
cabrerac <christiancabrera89@gmail.com>
2022-02-02 22:26:33 +0000

    fixing bugs reading script parameters

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from clients import service_registration
 
 
 def main(action, path):
-    if action == 'register_service':
+    if action == "register_service":
         service_registration.register_service(path)
     if action == "register_random_service":
         service_registration.register_random_service(path)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:], sys.argv[2:])
+    main(sys.argv[1], sys.argv[2])
